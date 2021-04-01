85f1bcc819c9499e4fdcd306068818b7a7d798b1
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2021-04-01 20:48:26 +0530

    [UPDATE] Caesar Cipher using std::array<double, ALPHABET_SIZE> instead of std::unordered_map<char, double>

--- Crypto/CaesarCipher/main.cpp
+++ Crypto/CaesarCipher/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string_view>
 #include <algorithm>
-#include <unordered_map>
+#include <array>
 
 #define ALPHABET_LENGTH 26
 #define FIRST 'a'
+//remaps ascii code of a...z from 97...123 to 0...25
+#define IDX(c) ((c) - FIRST)
 
 //takes an input string and a shift value(aka key)
 std::string caesar_encipher(std::string_view, int32_t);
@@ -43,27 +45,25 @@ std::string caesar_decipher(std::string_view cipher_txt, int32_t key) {
 std::string caesar_cipher_cracker(std::string str) {
     sanitize(str);
     size_t len = str.size();
+
 //    stealing frequency data from https://mitchellkember.com/blog/post/caesar-cipher/
-    constexpr double english_freqs[] = {
+    constexpr std::array<double, ALPHABET_LENGTH> english_freqs[] = {
             0.0804, 0.0148, 0.0334, 0.0382, 0.1249, 0.024, 0.0187, 0.0505, 0.0757, 0.0016,
             0.0054, 0.0407, 0.0251, 0.0723, 0.0764, 0.0214, 0.0012, 0.0628,
             0.0651, 0.0928, 0.0273, 0.0105, 0.0168, 0.0023, 0.0166, 0.0009
     };
-    std::unordered_map<char, double> message_freqs;
-    for (char c = FIRST + 0; c < FIRST + ALPHABET_LENGTH; ++c) {
-        message_freqs[c] = 0;
-    }
+
+    std::array<double, ALPHABET_LENGTH> message_fre{};
+
     for (const char &c : str) {
-        message_freqs[c]++;
+        message_fre[IDX(c)]++;
     }
 
-    for (auto&[k, v] : message_freqs)
-        v /= len;
+    for (double &i : message_fre)
+        i /= len;
 
     std::vector<double> chi_sqr_res(26);
 
-    for (auto&[k, v] : message_freqs)
-        std::cout << k << '\t' << v << '\n';
     return str;
 }
 
