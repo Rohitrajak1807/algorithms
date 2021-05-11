5151e02b627c55ba8ecb0e16e50491543f8fd9e4
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2021-05-11 23:21:36 +0530

    [UPDATE] Playfair Cipher A very rudimentary implementation of Playfair complete. Assumes no non alphabet in the message as well as the key. Only for english(please dont ask why, localization is a nightmare T_T)

--- Crypto/PlayfairCipher/main.cpp
+++ Crypto/PlayfairCipher/main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <array>
+#include <cassert>
 #include <caesar_cipher.h>
 
 // the dimension of playfair cipher lookup table is a 5x5
 #define TABLE_DIM 5
 
 #define ROW(idx) ((idx) / TABLE_DIM)
-#define COL(idx) ((idx) % TABLEDIM)
+#define COL(idx) ((idx) % TABLE_DIM)
 
 std::string playfair_encipher(std::string_view message, std::string_view key);
+
+std::string replace_all_j(std::string_view message);
 
 void fill_table(std::array<std::array<char, TABLE_DIM>, TABLE_DIM> &alphabet_table,
                 const std::array<bool, ALPHABET_LENGTH> &used_chars, size_t i, size_t j);
@@ -16,14 +19,22 @@ void fill_table(std::array<std::array<char, TABLE_DIM>, TABLE_DIM> &alphabet_tab
 std::array<size_t, ALPHABET_LENGTH> make_map(const std::array<std::array<char, TABLE_DIM>, TABLE_DIM> &used_chars);
 
 
+std::string form_digrams(const std::string &input);
+
 int main() {
-	std::string message = "attackatdawn";
+	std::string message = "hidethegoldinthetreestump";
 	std::string key = "playfairexample";
 	std::string cipher_text = playfair_encipher(message, key);
+	std::cout << cipher_text;
 	return 0;
 }
 
 std::string playfair_encipher(std::string_view message, std::string_view key) {
+
+//	we swap all j's in the message into i cause we consider them to be identical
+	std::string changed_message = replace_all_j(message);
+	std::string output;
+	output.reserve(message.size() * 2);
 	std::array<bool, ALPHABET_LENGTH> used_chars = {false};
 //	we assume i and j to be identical and as such 'j' never appears in the 5x5 grid, so
 //	marking it as a used character by default removes it from further consideration, Since
@@ -32,6 +43,8 @@ std::string playfair_encipher(std::string_view message, std::string_view key) {
 	used_chars[IDX('j')] = true;
 	std::array<std::array<char, TABLE_DIM>, TABLE_DIM> alphabet_table = {'\0'};
 	size_t i = 0, j = 0;
+
+//	fill the 5x5 grid used for encryption
 	for (const char &c : key) {
 		if (used_chars[IDX(c)])
 			continue;
@@ -48,16 +61,56 @@ std::string playfair_encipher(std::string_view message, std::string_view key) {
 
 	std::array<size_t, ALPHABET_LENGTH> alphabet_map = make_map(alphabet_table);
 
-	for(auto& r : alphabet_table) {
-		for(char &c : r)
-			std::cout << c << '\t';
-		std::cout << '\n';
+	changed_message = form_digrams(changed_message);
+
+	for (size_t idx = 0; idx < changed_message.size(); idx += 2) {
+		size_t curr = alphabet_map[IDX(changed_message[idx])];
+		size_t next = alphabet_map[IDX(changed_message[idx + 1])];
+		size_t r1 = ROW(curr),
+				r2 = ROW(next),
+				c1 = COL(curr),
+				c2 = COL(next);
+		if(r1 != r2 && c1 != c2) {
+			output.push_back(alphabet_table[r1][c2]);
+			output.push_back(alphabet_table[r2][c1]);
+		} else if(c1 == c2) {
+			output.push_back(alphabet_table[(r1 + 1) % TABLE_DIM][c1]);
+			output.push_back(alphabet_table[(r2 + 1) % TABLE_DIM][c2]);
+		} else if(r1 == r2) {
+			output.push_back(alphabet_table[r1][(c1 + 1) % TABLE_DIM]);
+			output.push_back(alphabet_table[r2][(c2 + 1) % TABLE_DIM]);
+		}
 	}
+	output.resize(output.size());
+	return output;
+}
 
-	for(size_t &s : alphabet_map)
-		std::cout << s << '\t';
+std::string form_digrams(const std::string &input) {
+	std::string new_str;
+	new_str.reserve(input.size() * 2);
+	for (size_t i = 0; i < input.size(); i += 2) {
+		const char &curr = input[i],
+				&next = (i + 1) >= input.size() ? '\0' : input[i + 1];
+		new_str.push_back(curr);
+		if (curr == next) {
+			new_str.push_back('x');
+		}
+		if (next != '\0')
+			new_str.push_back(next);
+	}
+	if (new_str.size() % 2 == 0) {
+		new_str[new_str.size()] = 'x';
+	}
+	new_str.resize(new_str.size());
+	return new_str;
+}
 
-	return "";
+std::string replace_all_j(std::string_view message) {
+	std::string new_str(message);
+	std::for_each(new_str.begin(), new_str.end(), [](char &c) {
+		c = (c == 'j') ? 'i' : c;
+	});
+	return new_str;
 }
 
 void fill_table(std::array<std::array<char, TABLE_DIM>, TABLE_DIM> &alphabet_table,
