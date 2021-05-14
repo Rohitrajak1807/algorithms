cdee4ee8258af178f1e644a91335fca6b1658af8
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2021-05-14 18:30:45 +0530

    [UPDATE] Rail fence cipher add comments reformat

--- Crypto/RailFenceCipher/main.cpp
+++ Crypto/RailFenceCipher/main.cpp
@@ -2,6 +2,7 @@
 #include <string_view>
 
 std::string rail_fence_encipher(std::string message, int32_t rails);
+
 int32_t rail_number(int32_t pos, int32_t rails);
 
 int main() {
@@ -15,17 +16,22 @@ int main() {
 std::string rail_fence_encipher(std::string message, int32_t rails) {
 	std::string cipher;
 	cipher.reserve(message.length());
-	for(int32_t i = 0; i < rails; ++i) {
-		for(int32_t j = 0; j < message.length(); ++j)
-			if(rail_number(j, rails) == i)
+	for (int32_t i = 0; i < rails; ++i) {
+		for (int32_t j = 0; j < message.length(); ++j)
+			if (rail_number(j, rails) == i)
 				cipher.push_back(message[j]);
 	}
 	return cipher;
 }
 
 int32_t rail_number(int32_t pos, int32_t rails) {
-	pos = pos % (2*rails - 2);
-	if(pos < rails)
+	/**
+	 * Note that the period for a given number of rails is 2*n - 1.
+	 * However, since the array indexing starts at 0 we need to compensate for it by
+	 * subtracting an extra 1. Mathematically pos = pos mod(period); where period = 2*rails - 1
+	 */
+	pos = pos % (2 * rails - 2);
+	if (pos < rails)
 		return pos;
 	return (2 * rails) - pos - 2;
 }
