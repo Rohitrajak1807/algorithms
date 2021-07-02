807cfe2adb1b1d0535633c2483dcbd16d33511c4
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2021-07-02 16:19:03 +0530

    [UPDATE] Travelling Salesman Approximate Reformat

--- ApproximationAlgorithms/TravellingSalesman/main.cpp
+++ ApproximationAlgorithms/TravellingSalesman/main.cpp
@@ -3,9 +3,10 @@
 
 std::vector<std::vector<Edge>> make_adj_list(const std::vector<Edge> &edges, size_t vertices);
 
-std::vector<int32_t> reconstruct_tour(const std::vector<std::vector<Edge>>& graph, int32_t start = 0);
+std::vector<int32_t> reconstruct_tour(const std::vector<std::vector<Edge>> &graph, int32_t start = 0);
 
-void dfs_impl(const std::vector<std::vector<Edge>> &graph, int32_t curr_node, std::vector<bool> &visited, std::vector<int32_t> &path);
+void dfs_impl(const std::vector<std::vector<Edge>> &graph, int32_t curr_node, std::vector<bool> &visited,
+              std::vector<int32_t> &path);
 
 int main() {
 	std::vector<std::vector<Edge>> g = {
@@ -16,24 +17,24 @@ int main() {
 			{Edge(4, 0, 20), Edge(4, 1, 12), Edge(4, 2, 25), Edge(4, 3, 30)},
 	};
 	int32_t start = 0;
-	auto&& [edges, cost] = lazy_jarnik_prim(g, start);
+	auto&&[edges, cost] = lazy_jarnik_prim(g, start);
 	std::vector<std::vector<Edge>> g_out = make_adj_list(edges, g.size());
-	auto&& path = reconstruct_tour(g_out);
-	for(int32_t& p : path) {
+	auto &&path = reconstruct_tour(g_out);
+	for (int32_t &p : path) {
 		std::cout << p << '\t';
 	}
-    return 0;
+	return 0;
 }
 
 std::vector<std::vector<Edge>> make_adj_list(const std::vector<Edge> &edges, size_t vertices) {
 	std::vector<std::vector<Edge>> graph(vertices);
-	for (const Edge& edge : edges) {
+	for (const Edge &edge : edges) {
 		graph[edge.from].emplace_back(edge);
 	}
 	return graph;
 }
 
-std::vector<int32_t> reconstruct_tour(const std::vector<std::vector<Edge>>& graph, int32_t start) {
+std::vector<int32_t> reconstruct_tour(const std::vector<std::vector<Edge>> &graph, int32_t start) {
 	std::vector<bool> visited(graph.size(), false);
 	std::vector<int32_t> path;
 	path.reserve(graph.size());
@@ -42,13 +43,14 @@ std::vector<int32_t> reconstruct_tour(const std::vector<std::vector<Edge>>& grap
 	return path;
 }
 
-void dfs_impl(const std::vector<std::vector<Edge>> &graph, int32_t curr_node, std::vector<bool> &visited, std::vector<int32_t> &path) {
-	if(visited[curr_node])
+void dfs_impl(const std::vector<std::vector<Edge>> &graph, int32_t curr_node, std::vector<bool> &visited,
+              std::vector<int32_t> &path) {
+	if (visited[curr_node])
 		return;
 	visited[curr_node] = true;
 	path.emplace_back(curr_node);
-	auto& neighbours = graph[curr_node];
-	for(auto &neighbour : neighbours) {
+	auto &neighbours = graph[curr_node];
+	for (auto &neighbour : neighbours) {
 		dfs_impl(graph, neighbour.to, visited, path);
 	}
 }
